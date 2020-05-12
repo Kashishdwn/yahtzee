9e7599325f546c4b6fc49908f253a0d70ebb3533
Kashishdwn <kashishdwn@gmail.com>
2020-05-13 01:47:28 +0530

    Updates

---
+++ precache-manifest.edeec11e51aca673bc905881fc114658.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829d824d4f4bd8016609ccbcb92230ec",
+    "url": "/yahtzee/index.html"
+  },
+  {
+    "revision": "a9baa828fb5a0e0eaf5b",
+    "url": "/yahtzee/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35070078b129d5e7b719",
+    "url": "/yahtzee/static/js/2.5acaca58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yahtzee/static/js/2.5acaca58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9baa828fb5a0e0eaf5b",
+    "url": "/yahtzee/static/js/main.7dcf6690.chunk.js"
+  },
+  {
+    "revision": "234ed74a85f314829608",
+    "url": "/yahtzee/static/js/runtime-main.21b093a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yahtzee/static/media/logo.5d5d9eef.svg"
+  }
+]);
