2d61156724c43e7823297e84ccbfe54a4ccb1f31
Kashishdwn <kashishdwn@gmail.com>
2020-05-15 20:44:17 +0530

    Updates

--- precache-manifest.280d124e4148de89258dc2950223b35b.js
+++ precache-manifest.280d124e4148de89258dc2950223b35b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "52b797f54cc7697f045a4fe7bd9430f3",
+    "revision": "8c1e45dfaa23d411b8ecaf5f4bf1ec9c",
     "url": "/yahtzee/index.html"
   },
   {
-    "revision": "d4fb264cbef45475b5c0",
+    "revision": "67291c4e00e04a3d2e7c",
     "url": "/yahtzee/static/css/main.525cb608.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/yahtzee/static/js/2.3510c84e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d4fb264cbef45475b5c0",
-    "url": "/yahtzee/static/js/main.270bcc7d.chunk.js"
+    "revision": "67291c4e00e04a3d2e7c",
+    "url": "/yahtzee/static/js/main.b37e6cf5.chunk.js"
   },
   {
     "revision": "234ed74a85f314829608",
